58af006120953c61aacabc4460df6238007d35b7
Yi <Devindopa@gmail.com>
2017-09-23 12:16:26 +1000

    add Coord Class

---
+++ src/Coord.java
@@ -0,0 +1,21 @@
+/**
+ * Created by zy on 23/09/2017.
+ */
+public class Coord {
+
+    private int xValue;
+    private int yValue;
+
+    Coord(int xValue, int yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+    }
+
+    public int x() {
+        return xValue;
+    }
+
+    public int y() {
+        return yValue;
+    }
+}
