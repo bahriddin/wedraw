c9cc5143a7c76b5d5be9ce40db8501f3a01f2d18
LuanBoheng <314887027@qq.com>
2017-09-25 11:55:30 +1000

    simple demo of mouse listener

---
+++ src/MouseEventDemo.java
@@ -0,0 +1,78 @@
+import java.awt.GridLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.*;
+
+public class MouseEventDemo extends JPanel {
+
+    WhiteBoard whiteBoard;
+    public static JTextArea textArea;
+
+    public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+        //Schedule a job for the event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event dispatch thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("MouseEventDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        JComponent newContentPane = new MouseEventDemo();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MouseEventDemo() {
+        super(new GridLayout(0,1));
+        whiteBoard = new WhiteBoard(Color.WHITE);
+        add(whiteBoard);
+        textArea = new JTextArea();
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setPreferredSize(new Dimension(200, 75));
+        add(scrollPane);
+
+        //Register for mouse events on whiteBoard and the panel.
+        MouseEvents Mouse = new MouseEvents();
+        whiteBoard.addMouseListener(Mouse);
+        whiteBoard.addMouseMotionListener(Mouse);
+        addMouseListener(Mouse);
+        addMouseMotionListener(Mouse);
+        setPreferredSize(new Dimension(1024, 800));
+        setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+    }
+
+}
---
+++ src/WhiteBoard.java
@@ -0,0 +1,23 @@
+
+
+import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class WhiteBoard extends JLabel {
+    Dimension minSize = new Dimension(100, 50);
+
+    public WhiteBoard(Color color) {
+        setBackground(color);
+        setOpaque(true);
+        setBorder(BorderFactory.createLineBorder(Color.black));
+    }
+
+    public Dimension getMinimumSize() {
+        return minSize;
+    }
+
+    public Dimension getPreferredSize() {
+        return minSize;
+    }
+}
