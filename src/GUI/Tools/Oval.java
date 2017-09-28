e2e2a99cb2f17ffff58afff3756bc04b2ace4ecd
LuanBoheng <314887027@qq.com>
2017-09-28 21:08:52 +1000

    change Oval

--- src/GUI/Tools/Oval.java
+++ src/GUI/Tools/Oval.java
@@ -1,7 +1,27 @@
 package GUI.Tools;
 
-/**
- * Created by luanboheng on 28/09/2017.
- */
-public class Oval {
+import Data.Coord;
+import GUI.DrawSettings.DrawSettings;
+import GUI.Layout.CanvasArea;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Oval extends Shape{
+    public Oval(CanvasArea canvasArea) {
+        super(canvasArea);
+    }
+
+
+
+    void startDraw(){
+        this.model.startDrawOval(start,end,color,lineStyle,isFilled);
+    }
+    void continueDraw(){
+        this.model.continueDrawOval(start,end,color,lineStyle,isFilled);
+    }
+    void endDraw(){
+        this.model.stopDrawOval(start,end,color,lineStyle,isFilled);
+    }
 }
